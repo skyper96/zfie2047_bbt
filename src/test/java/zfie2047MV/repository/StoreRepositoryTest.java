cb66d7197cfbcbded144b73a2a17478d0f32e3ec
Filip Zirbo <phil_zir@yahoo.com>
2018-03-23 18:59:40 -0700

    test 1

--- src/test/java/zfie2047MV/repository/StoreRepositoryTest.java
+++ src/test/java/zfie2047MV/repository/StoreRepositoryTest.java
@@ -1,10 +1,17 @@
 package zfie2047MV.repository;
 
+import zfie2047MV.model.Product;
+
 import static org.junit.Assert.*;
 
 public class StoreRepositoryTest {
 
     @org.junit.Test
     public void addNewProduct() {
+        Product p = new Product();
+        p.setName("lapte");
+        p.setCode(8);
+        p.setCategory("alimente");
+        p.setQuantity(10);
     }
 }
